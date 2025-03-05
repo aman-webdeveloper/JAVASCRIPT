4922dbe6e40c38cfb9f0809e47bd2613cc004dfe
Aman Kumar <amnkumar4512@gmail.com>
2025-03-05 10:56:40 +0530

    add files

---
+++ JS/6datatypeConversion.js
@@ -0,0 +1,15 @@
+console.log("Hello world");
+
+//"33 =>" 33
+// "33abc" => NaN
+// true => 1;
+// false => 0;
+
+// "" => false
+// "aman" => true
+
+let somenumber = 33
+let stringNumber = String(somenumber);
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
