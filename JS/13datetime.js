88ceda92aaf428ac5e408ce6a375a0e1f8c21227
Aman Kumar <amnkumar4512@gmail.com>
2025-03-08 11:37:35 +0530

    New Files Uploaded

---
+++ JS/13datetime.js
@@ -0,0 +1,29 @@
+
+// Date
+
+// let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+//TIME AND DATE  FORMAT ----------------------------------------
+
+// let mycreateDate = new Date (2023 , 0 , 23);
+// let mycreateDate1 = new Date (2023 , 0 , 23,5,3);
+let mycreateDate2 = new Date ("2023-01-14");
+// let mycreateDate3 = new Date ("10-21-2005");
+// console.log(mycreateDate.toDateString());
+// console.log(mycreateDate1.toDateString());
+console.log(mycreateDate2.toDateString());
+// console.log(mycreateDate3.toDateString());
+
+let mytimestamp = Date.now();
+// console.log(mytimestamp);
+//  console.log(mycreateDate2.getTime());
+console.log(Math.floor(Date.now()/1000));
+ 
+
+
+
+
